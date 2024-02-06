34e1906c50d3755672c345958ac39ab230cb8003
ziad harb <ziadharb@ziads-Air.lan>
2024-02-05 19:30:00 -0600

    modify test

--- cypress/e2e/first-Test.cy.js
+++ cypress/e2e/first-Test.cy.js
@@ -1,5 +1,9 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
+    cy.get('[data-test-id="Skywalker,Anakin"]').should(
+      "have.text",
+      "Skywalker,Anakin"
+    );
   })
 })
